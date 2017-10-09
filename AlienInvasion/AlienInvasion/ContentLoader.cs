407153b5996e55b27af575ae0636b9f3c8fb3ef5
Belbertn <belbert95@me.com>
2017-10-10 00:21:38 +0200

    ContentLoader

---
+++ AlienInvasion/AlienInvasion/ContentLoader.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace AlienInvasion
+{
+
+    public class ContentLoader
+    {
+        private static Dictionary<string, Texture2D> Sprites { get; set; }
+        private ContentManager content;
+
+        public void Initialize(ContentManager Content)
+        {
+            Sprites = new Dictionary<string, Texture2D>();
+            content = Content;
+        }
+
+        public void LoadAssets(string filename)
+        {
+            string[] lines = ReadFile(filename);
+
+            if (filename.Contains("Texture"))
+            {
+                LoadSprites(lines);
+            }
+        }
+
+        public void UnLoadAssets(string filename)
+        {
+            string[] lines = ReadFile(filename);
+
+            if (filename.Contains("Texture"))
+            {
+                UnLoadSprites(lines);
+            }
+        }
+
+        public static Texture2D GetSprite(string name)
+        {
+            if (Sprites.ContainsKey(name))
+            {
+                return Sprites[name];
+            }
+            else if (Sprites.Count <= 0)
+            {
+                throw new System.ArgumentException("Dictionary is empty at this time. Maybe it's not finished loading");
+            }
+            else
+            {
+                throw new System.ArgumentException(name + " Sprite does not exist");
+            }
+        }
+
+        public static string[] GetAssetFile(string fileName)
+        {
+            return File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName));
+        }
+
+        private string[] ReadFile(string TextFile)
+        {
+            string[] lines = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, TextFile));
+
+            return lines;
+        }
+
+        private void LoadSprites(string[] lines)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (!lines[i].Contains("animation") && !Sprites.ContainsKey(lines[i]))
+                {
+                    Sprites.Add(lines[i], content.Load<Texture2D>(lines[i]));
+                }
+            }
+        }
+
+        private void UnLoadSprites(string[] lines)
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Sprites.Remove(lines[i]);
+            }
+        }
+    }
+}
