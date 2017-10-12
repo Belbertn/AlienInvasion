4150f7dca5bb094dc9a3edf5b5990fd8edd15989
Belbertn <belbert95@me.com>
2017-10-13 00:31:34 +0200

    sd

---
+++ AlienInvasion/AlienInvasion/IGameState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace AlienInvasion
+{
+    interface IGameState
+    {
+        void Initialize(Alien a, SpriteBatch spriteBatch);
+        void Load(ContentManager content);
+        void Update(GameTime gameTime);
+        void Draw();
+    }
+}
