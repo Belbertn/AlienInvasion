bff93ba69d7d8408def0f2e0b30f3529890ff0b3
Belbertn <belbert95@me.com>
2017-10-13 16:20:33 +0200

    fsdf

--- AlienInvasion/AlienInvasion/IEntity.cs
+++ AlienInvasion/AlienInvasion/IEntity.cs
@@ -9,14 +9,14 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AlienInvasion
 {
-    interface IGameState
+    interface IEntity
     {
+        Vector2 Position { get; set; }
         void Initialize(Alien a, SpriteBatch spriteBatch);
         void Load();
         void Update(GameTime gameTime);
---
+++ AlienInvasion/AlienInvasion/InGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace AlienInvasion
+{
+    class InGame : IGameState
+    {
+        private Alien a;
+        private SpriteBatch spriteBatch;
+
+        public List<IEntity> Entities { get; set; }
+
+        public void Initialize(Alien a, SpriteBatch spriteBatch)
+        {
+            this.a = a;
+            this.spriteBatch = spriteBatch;
+
+            Entities = new List<IEntity>();
+        }
+
+        public void Load()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
